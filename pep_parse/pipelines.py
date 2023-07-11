5ae5f393a5cbcfa70e4f4ab43158c9d759706e7f
Alex386386 <mamonov40600@mail.ru>
2023-07-11 14:25:28 +0300

    review #0.1

---
+++ pep_parse/pipelines.py
@@ -0,0 +1,30 @@
+import csv
+import datetime
+from collections import defaultdict
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent / 'results'
+
+
+class PepParsePipeline:
+    def open_spider(self, spider):
+        self.status_counts = defaultdict(int)
+
+    def process_item(self, item, spider):
+        self.status_counts[item['status']] += 1
+        return item
+
+    def close_spider(self, spider):
+        date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        BASE_DIR.mkdir(exist_ok=True)
+        filename = BASE_DIR / f'status_summary_{date}.csv'
+        with open(filename, 'w', newline='') as csvfile:
+            fieldnames = ['Status', 'Count']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            for status, count in self.status_counts.items():
+                writer.writerow({'Status': status, 'Count': count})
+
+            writer.writerow({'Status': 'Total', 'Count': sum(
+                self.status_counts.values())})
---
+++ pep_parse/spiders/pep.py
@@ -0,0 +1,35 @@
+from urllib.parse import urljoin
+
+import scrapy
+
+from pep_parse.items import PepParseItem
+
+
+class PepSpider(scrapy.Spider):
+    name = 'pep'
+    allowed_domains = ['peps.python.org']
+    start_urls = ['https://peps.python.org/']
+
+    def parse(self, response):
+        numerical = response.xpath('//*[@id="numerical-index"]')
+        tab_body = numerical.css('tbody')
+        tab_rows = tab_body.css('tr').getall()
+        for tab_row in tab_rows:
+            row = scrapy.Selector(text=tab_row)
+            link = row.css('a::attr(href)').get()
+            pep_link = urljoin(self.start_urls[0], link)
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        number = response.xpath(
+            '//*[@id="pep-page-section"]/header/ul/li[3]').re('\d+')[0]
+        name = response.css('h1.page-title::text').get()
+        status = response.xpath(
+            'string(//dt[contains(text(), "Status")]/following-sibling::dd[1])'
+        ).get()
+        data = {
+            'number': number,
+            'name': name,
+            'status': status,
+        }
+        yield PepParseItem(data)
